445190587b4cc72faba9cbdd2104854b9e8c8d7e
harukaxq <h.tamura@cibo.design>
2024-09-30 15:46:40 +0900

    othello

--- templates/othello_template/src/routes/+page.svelte
+++ templates/othello_template/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Svelteのサンプルです</h1>
+<h2>まずはSvelteの使い方を学びましょう</h2>
+<p>Svelteはフロントエンドフレームワークです。</p>
